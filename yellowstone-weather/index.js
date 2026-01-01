9dc19e82b7f779ebc4db17eddb4c5bd9b6f35699
jaffa <jaffasdev@gmail.com>
2026-01-01 16:33:18 +0000

    s
    s

--- yellowstone-weather/index.js
+++ yellowstone-weather/index.js
@@ -23,4 +23,9 @@ app.get("/weather", async (req, res) => {
     }
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
+
